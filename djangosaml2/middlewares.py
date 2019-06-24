7cf8b4bca44a91cedba318818b2a7cb06a40b458
Vijith <vijith@instamojo.com>
2019-06-24 16:57:17 +0530

    looping issue fix

--- djangosaml2/middlewares.py
+++ djangosaml2/middlewares.py
@@ -3,9 +3,9 @@ import logging
 from django.conf import settings
 from django.contrib.auth import logout
 from django.core.exceptions import ImproperlyConfigured
-from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils.deprecation import MiddlewareMixin
+from djangosaml2.views import login as saml_login
 
 logger = logging.getLogger(__name__)
 
@@ -21,13 +21,21 @@ class HintCookieMiddleware(MiddlewareMixin):
             raise ImproperlyConfigured(
                 "SAML_HINT_COOKIE_NAME not defined on use of HintCookieMiddleware")
 
+        # To avoid a loop
+        # saml2_login eventually redirects to saml2_acs
+        if request.path == reverse('saml2_login') or request.path == reverse('saml2_acs'):
+            return None
+
         saml_hint_cookie = request.COOKIES.get(hint_cookie_name)
-        if saml_hint_cookie:
-            # Automatically do an SP initiated login
+
+        # If logged out and the hint cookie is available
+        # automatically do an SP initiated login
+        if not request.user.is_authenticated and saml_hint_cookie:
             logger.info('SAML: Hint cookie present. Initiating an auto login.')
-            return HttpResponseRedirect(reverse('djangosaml2:login'))
-        else:
-            # Assuming the user would have logged out in the IdP
-            # Logout the user
+            return saml_login(request)
+
+        # If logged in and the hint cookie is missing
+        # automatically do a logout
+        if request.user.is_authenticated and not saml_hint_cookie:
             logger.info('SAML: Hint cookie missing. Initiating an auto logout.')
             logout(request)
