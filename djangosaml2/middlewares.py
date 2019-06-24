c6de400e9cf1f7a45ba6853bf5442d2fff5f0a32
Vijith <vijith@instamojo.com>
2019-06-24 06:59:27 +0530

    Adding a middleware for hint cookie

---
+++ djangosaml2/middlewares.py
@@ -0,0 +1,31 @@
+import logging
+
+from django.conf import settings
+from django.contrib.auth import logout
+from django.core.exceptions import ImproperlyConfigured
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.deprecation import MiddlewareMixin
+
+logger = logging.getLogger(__name__)
+
+
+class HintCookieMiddleware(MiddlewareMixin):
+
+    def process_request(self, request):
+        try:
+            hint_cookie_name = settings.SAML_HINT_COOKIE_NAME
+        except AttributeError:
+            raise ImproperlyConfigured(
+                "SAML_HINT_COOKIE_NAME not defined on use of HintCookieMiddleware")
+
+        saml_hint_cookie = request.COOKIES.get(hint_cookie_name)
+        if saml_hint_cookie:
+            # Automatically do an SP initiated login
+            logger.info('SAML: Hint cookie present. Initiating an auto login.')
+            return HttpResponseRedirect(reverse('djangosaml2:login'))
+        else:
+            # Assuming the user would have logged out in the IdP
+            # Logout the user
+            logger.info('SAML: Hint cookie missing. Initiating an auto logout.')
+            logout(request)
